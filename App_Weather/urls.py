cc38e5bccfbf97b371f09589a6d7c5a197c01d71
gbolahan <thegbolahanalaba@gmail.com>
2024-08-05 08:18:25 +0100

    first

--- App_Weather/urls.py
+++ App_Weather/urls.py
@@ -4,8 +4,6 @@ from rest_framework.routers import DefaultRouter
 from . views import *
 
 
-
-
 urlpatterns = [ 
    path('weather', WeatherViewSet.as_view({"post": "create"}), name='weather'), # weather
    path('weather/<str:city>', WeatherViewSet.as_view({"get": "retrieve_city"}), name='weather'), # weather
